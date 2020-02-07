cdabbbad3359f074575960115d9f8d737dc6dd1e
LustrousLizard <coolcatsteelst@gmail.com>
2020-02-07 14:36:55 -0800

    playerhealth nowhere near done yet tho

---
+++ Mountain’s Heart/Assets/_Scripts/PlayerHealthController.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerHealthController : MonoBehaviour
+{
+    private List<GameObject> playerHealth;
+    public int healthValue;
+
+    void Update()
+    {
+        if(healthValue % 2 == 0)
+        {
+            for(int i = 0; i < healthValue; i++)
+            {
+                playerHealth[i] 
+            }
+        }
+    }
+
+}
