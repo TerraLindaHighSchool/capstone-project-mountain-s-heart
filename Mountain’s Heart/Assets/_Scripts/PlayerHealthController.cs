16c0307dee8d5eba51d54a962b135bca24fd14d6
LustrousLizard <coolcatsteelst@gmail.com>
2020-02-12 14:33:54 -0800

    th

--- Mountain’s Heart/Assets/_Scripts/PlayerHealthController.cs
+++ Mountain’s Heart/Assets/_Scripts/PlayerHealthController.cs
@@ -4,19 +4,6 @@ using UnityEngine;
 
 public class PlayerHealthController : MonoBehaviour
 {
-    private List<GameObject> playerHealth;
-    public int healthValue;
 
-    void Update()
-    {
-        if(healthValue % 2 == 0)
-        {
-            for(int i = 0; i < healthValue; i++)
-            {
-                //playerHealth[i] 
-
-            }
-        }
-    }
 
 }
