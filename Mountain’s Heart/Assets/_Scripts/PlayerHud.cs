dbf58f8a4ebd6ea9351ba4fca6d64cba0e239623
LustrousLizard <coolcatsteelst@gmail.com>
2020-02-14 14:28:24 -0800

    player hud

---
+++ Mountain’s Heart/Assets/_Scripts/PlayerHud.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.Sprites;
+
+public class PlayerHud : MonoBehaviour
+{
+
+    [SerializeField] private Image[] healthBar;
+    [SerializeField] private Sprite[] healthState;
+    private GameObject player;
+    private int playerHealth;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        player = GameObject.FindGameObjectWithTag("Player");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        playerHealth = player.GetComponent<PlayerHealthController>().getHealth();
+
+        if (healthBar[playerHealth].sprite == healthState[2])
+            healthBar[playerHealth].sprite = healthState[0];
+        if (healthBar[playerHealth].sprite == healthState[4])
+                healthBar[playerHealth].sprite = healthState[2];
+    }
+}
