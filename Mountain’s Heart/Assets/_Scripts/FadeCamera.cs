a3e4843a933ee74303e4a17817289a7ddbcea288
LustrousLizard <coolcatsteelst@gmail.com>
2020-03-04 14:36:08 -0800

    Camera fades in and out when scene changes

---
+++ Mountain’s Heart/Assets/_Scripts/FadeCamera.cs
@@ -0,0 +1,64 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class FadeCamera : MonoBehaviour
+{
+    [SerializeField] private Image blackScreen;
+    private float currentAlpha;
+    private bool blek = false;
+    private bool turnBlek = false;
+    private bool turnKlere = false;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        turnKlere = true;
+        blackScreen.enabled = true;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (turnBlek)
+        {
+            currentAlpha = blackScreen.color.a;
+            blackScreen.color = new Color(0, 0, 0, currentAlpha + 0.02f);
+            if (blackScreen.color.a >= 1)
+            {
+                blek = true;
+                turnBlek = false;
+            }
+        }
+
+        if (turnKlere)
+        {
+            currentAlpha = blackScreen.color.a;
+            blackScreen.color = new Color(0, 0, 0, currentAlpha - 0.02f);
+            if (blackScreen.color.a <= 0)
+            {
+                blek = false;
+                turnKlere = false;
+            }
+        }
+    }
+
+
+    public bool isBlek()
+    {
+        return blek;
+    }
+
+    public void setTurnBlek(bool lol)
+    {
+        turnBlek = lol;
+    }
+
+    public void setTurnKlere(bool lol)
+    {
+        turnKlere = lol;
+
+    }
+}
--- Mountain’s Heart/Assets/_Scripts/LoadRespectiveScene.cs
+++ Mountain’s Heart/Assets/_Scripts/LoadRespectiveScene.cs
@@ -2,19 +2,24 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 
 public class LoadRespectiveScene : MonoBehaviour
 {
     [SerializeField] private Object scene;
-
+    [SerializeField] FadeCamera fader;
 
     private void OnCollisionEnter2D(Collision2D collision)
     {
-        Debug.Log("hit");
-        //if (collision.gameObject.tag.Equals("Player"))
-       // {
-            Debug.Log("on");
+        if (collision.gameObject.tag.Equals("Player"))
+        {
+            fader.setTurnBlek(true);
+        }
+    }
+
+    private void Update()
+    {
+        if (fader.isBlek())
             SceneManager.LoadScene(sceneName: scene.name);
-       // }
     }
 }
