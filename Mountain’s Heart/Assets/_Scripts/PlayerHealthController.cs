0fa955101ca2e8d0cef40249a455c377b5e99dab
Wil C Bauer <wb00188@srcs.loc>
2020-03-13 14:14:00 -0700

    player invincibility on damage and updated hud

--- Mountain’s Heart/Assets/_Scripts/PlayerHealthController.cs
+++ Mountain’s Heart/Assets/_Scripts/PlayerHealthController.cs
@@ -9,6 +9,8 @@ public class PlayerHealthController : MonoBehaviour
     private int health = 8;
     private Rigidbody2D rb;
     private Vector2 recoil;
+    private bool invincible = false;
+    private int countdown = 60;
     [SerializeField] float recoilModifier = 30;
 
     private void Start()
@@ -18,11 +20,12 @@ public class PlayerHealthController : MonoBehaviour
 
     private void OnCollisionEnter2D(Collision2D collision)
     {
-        if (collision.gameObject.GetComponent<EnemyStats>() != null)
+        if (collision.gameObject.GetComponent<EnemyStats>() != null && !invincible)
         {
             health -= collision.gameObject.GetComponent<EnemyStats>().getDamage();
             recoil = new Vector2(recoilModifier * (this.transform.position.x - collision.gameObject.transform.position.x),
                                     recoilModifier * (this.transform.position.y - collision.gameObject.transform.position.y));
+            invincible = true;
             GetComponent<Rigidbody2D>().AddForce(recoil);
 
             if (health < -1)
@@ -37,6 +40,17 @@ public class PlayerHealthController : MonoBehaviour
 
     private void Update()
     {
+        if (invincible)
+        {
+            countdown--;
+            if(countdown <= 0)
+            {
+                invincible = false;
+                countdown = 60;
+            }
+        }
+
+
         if (health < 0)
             SceneManager.LoadScene(sceneName: "Main Menu");
     }
